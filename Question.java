bfec10f56b5af982800e13777128acb42f5f0661
varrelkusuma <varrelksm@gmail.com>
2018-03-23 22:51:42 +0700

    Question.java

--- Question.java
+++ Question.java
@@ -2,6 +2,11 @@ public class Question {
   private String question;
   private String answer;
 
+  public Question(String question, String answer) {
+    this.question = question;
+    this.answer = answer;
+  }
+
   public String getQuestion() {
     return this.question;
   }

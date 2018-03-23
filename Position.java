686f8e4575a77f270c9d0e7dd1a0ad67be3d4e2f
theRealSandro <assandroa95@gmail.com>
2018-03-23 22:40:27 +0700

    Position
    Position

--- Position.java
+++ Position.java
@@ -1,33 +1,27 @@
 // Position.java
 
 class Position {
-  private double x;
-  private double y;
+  private int x;
+  private int y;
 
   /** CTOR, CCTOR */
   public Position() { x = 0; y = 0; }
-  public Position(double _x, double _y) { x = _x; y = _y; }
+  public Position(int _x, int _y) { x = _x; y = _y; }
   public Position(final Position p) { x = p.x; y = p.y; }
 
   /** GETTER, SETTER */
-  public final double getX() { return x; }
-  public final double getY() { return y; }
-  public void setX(double _x) { x = _x; }
-  public void setY(double _y) { y = _y; }
+  public final int getX() { return x; }
+  public final int getY() { return y; }
+  public void setX(int _x) { x = _x; }
+  public void setY(int _y) { y = _y; }
 
   public String toString() { return "(" + x + "," + y + ")"; }
-
-  public double distanceTo(Position p) {
-    double dx = x - p.x;
-    double dy = x - p.y;
-    return Math.sqrt(dx*dx + dy*dy);
-  }
 
   public boolean equals(Position p) { 
     return x == p.x && y == p.y; 
   }
 
-  public void move(double dx, double dy) { 
+  public void move(int dx, int dy) { 
     x += dx; y += dy; 
   }
 

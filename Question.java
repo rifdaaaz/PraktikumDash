62bbe222b3c4e29414b8c96945154fbdceb3816f
varrelkusuma <varrelksm@gmail.com>
2018-03-23 22:48:39 +0700

    question
    udah selese

---
+++ Question.java
@@ -0,0 +1,20 @@
+public class Question {
+  private String question;
+  private String answer;
+
+  public String getQuestion() {
+    return this.question;
+  }
+
+  public String getAnswer() {
+    return this.answer;
+  }
+
+  public void setQuestion(String question) {
+    this.question = question;
+  }
+
+  public void setAnswer(String answer) {
+    this.answer = answer;
+  }
+}

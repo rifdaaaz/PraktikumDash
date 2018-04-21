a1eeb1e2335cb716a4b5444cd3a739c3c55b649f
Athurnm <athurnm@gmail.com>
2018-04-21 12:26:01 +0700

    Fix git, and added Peta and View

---
+++ Peta.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+public class Peta {
+    private char[][] peta = new char[14][14];
+    private static Peta petaInstance = null;
+
+    public static Peta getInstance () {
+        if (petaInstance == null){
+            petaInstance = new Peta();
+        }
+        return petaInstance;
+    }
+
+    private Peta() {
+        for (int i = 0; i < 14; i ++) {
+            this.peta[i][0] = '#';
+            this.peta[i][13] = '#';
+            this.peta[0][i] = '#';
+            this.peta[13][i] = '#';
+        } 
+        for (int i = 1; i < 13; i++) {
+            for (int j = 1; j < 13; j ++) {
+                this.peta[i][j] = '.';
+            }
+        }
+    }
+
+    public void setPeta(char [][] maze){
+        for (int i = 0; i<14; i++) {
+            for (int j = 0; j<14; j++) {
+                peta[i][j] = maze[i][j];
+            }
+        }
+    }
+
+    public void printPeta() {
+        for (int i = 0; i < 14; i++) {
+            for (int j = 0; j < 14; j ++) {
+                System.out.print(peta[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public char[][] get() {
+        return peta;
+    }
+
+    public synchronized void placeAsisten(Asisten asisten) {
+        peta[asisten.getPos().getX()][asisten.getPos().getY()] = asisten.getLogo();
+    }
+
+    public synchronized void placePraktikan(Praktikan praktikan) {
+        peta[praktikan.getPos().getX()][praktikan.getPos().getY()] = praktikan.getLogo();
+    }
+
+    public synchronized void placeWall(Position place) {
+        peta[place.getX()][place.getY()] = '#';
+    }
+
+    public synchronized void setTitik(Position place) {
+        peta[place.getX()][place.getY()] = '.';
+    }
+
+    public synchronized void setKoor(char logo, Position place) {
+        peta[place.getX()][place.getY()] = logo;
+    }
+
+    // public static void main (String[] args){
+    //     Peta worldMap = Peta.getInstance();
+    //     worldMap.printPeta1();
+    // }
+
+}
---
+++ View.java
@@ -0,0 +1,122 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
+
+public class View extends JFrame {
+	JPanel jp = new JPanel();
+	JLabel jl = new JLabel("ini pertanyaannyaaaa");
+	JTextField jt = new JTextField("Enter your answer here...",30);
+	JButton jb = new JButton("Enter");
+
+	// convention: board[row][col]
+
+	// values: 0 = not visited node
+	// 		1 = wall
+	// 		2 = visited node
+	// 9 = target node
+
+	//start at 1,1
+	 private char[][] board=
+	{ {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+	  {'#','.','.','.','.','.','.','.','.','.','.','.','.','#','#'},
+	  {'#','.','.','.','.','.','.','.','.','.','.','.','.','#','#'},
+	  {'#','.','.','.','.','.','.','.','.','.','.','.','.','#','#'},
+	  {'#','.','.','.','.','.','.','.','.','.','.','.','.','#','#'},
+	  {'#','.','.','.','.','.','.','.','.','.','.','.','.','#','#'},
+	  {'#','.','.','.','.','.','.','.','.','.','.','.','.','#','#'},
+	  {'#','.','.','.','#','.','.','.','.','.','.','.','.','#','#'},
+	  {'#','.','.','.','.','.','.','.','.','.','.','.','.','#','#'},
+	  {'#','.','.','.','.','.','.','.','.','.','.','.','.','#','#'},
+	  {'#','.','.','.','.','.','.','.','.','.','.','.','.','#','#'},
+	  {'#','.','.','.','.','.','.','.','.','.','.','.','.','#','#'}, 
+	  {'#','.','.','.','.','.','.','.','.','.','.','.','.','#','#'},
+	  {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+	  {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'}   
+	};
+
+
+	 public View() {
+	 		setTitle("Praktikum Dash!");
+	 		setSize(520,600);
+	 		setLocationRelativeTo(null);
+	 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 		//jt.setLocationRelativeTo()
+
+	 		//======= Kasih Pertanyaan =====================
+
+	 		jl.setText("ini diganti pertanyaannya");
+
+	 		//======= BUAT INPUTAN DR KEYBOARD =============
+
+
+	 		//this.setLayout(null);
+	 		//jp.setLayout(null);
+	 		jb.setBounds(300,500,100,200);
+	 		jp.add(jt);
+	 		jp.add(jb);
+	 		jp.add(jl);
+	 		jb.addActionListener(new ActionListener(){
+ 				public void actionPerformed(ActionEvent ae){
+ 					String input = jt.getText();
+ 					System.out.println("Input: "+input);
+ 				}
+	 		});
+	 		//jp.setBackground(Color.BLACK);
+	 		add(jp);
+	 		jb.setLocation(500,100);
+
+	 		//=== end==
+
+	 }
+
+ 	@Override
+ 	public void paint(Graphics g) {
+ 		super.paint(g);
+ 		g.translate(50,50);
+
+ 		//gambar boardnya
+ 		for (int row=0; row<(board.length-1); row++) {
+ 			for (int col=0; col<(board[0].length-1); col++) {
+ 				Color color;
+ 				switch (board[row][col]) {
+ 					case '#': color = Color.BLACK; break;
+ 					case '.': color = Color.WHITE; break;
+ 					case '1': color = Color.RED; break;
+ 					case 'A': color = Color.GREEN; break;
+ 					case 'B': color = Color.BLUE; break;
+ 					default : color = Color.RED;
+ 				}
+ 				g.setColor(color);
+ 				g.fillRect(30*col,30*row,30,30);
+ 				g.setColor(Color.BLACK);
+ 				g.drawRect(30*col,30*row,30,30);
+
+ 			}
+ 		}
+ 	}
+
+ 	public void setView (char[][] map){
+ 		this.board=map;
+ 	}
+	 public static void main (String[] args){
+	 	SwingUtilities.invokeLater(new Runnable() {
+
+	 		@Override
+	 		public void run() {
+	 			View view = new View();
+	 			view.setVisible(true);
+	 			view.repaint();
+	 				 		}
+	 	});
+	 } 
+}
